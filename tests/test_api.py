2a5721fe4fc0e5a462177ce41e64a7da45a0f894
alif_ramadhan <alif@skinmystery.com>
2024-07-28 03:48:57 +0700

    add: documentation specs swagger:

--- tests/test_api.py
+++ tests/test_api.py
@@ -40,7 +40,8 @@ def hit_transportation_recommendation(url: str, query: str, lon: float = 106.782
 
 # Example usage:
 if __name__ == "__main__":
-    url = "http://localhost:8000/generate-recommendation"
+    # url = "http://localhost:8000/generate-recommendation"
+    url = "http://170.64.228.233:8000/generate-recommendation"
     text_result = hit_text_recommendation(url, "Nama: Alif Ramadhan, aku laper banget pengen makan yang manis-manis dengan budget 50$")
     text_result = hit_text_recommendation(url, "Nama: Alif Ramadhan, aku laper banget pengen makan")
     image_result = hit_image_recommendation(url, "static/keju.jpg")
