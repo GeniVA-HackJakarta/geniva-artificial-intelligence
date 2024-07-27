fc1a1cecf69a7bdb0ede565b65bf281a09c1a8f0
alif_ramadhan <alif@skinmystery.com>
2024-07-28 03:38:01 +0700

    delete: test.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -30,14 +30,19 @@ def hit_multimodal_recommendation(url: str, query: str, file_path: str):
     else:
         response.raise_for_status()
 
+def hit_transportation_recommendation(url: str, query: str, lon: float = 106.7829313, lat: float = -6.2847815):
+    payload = {"query": query, "lon": lon, "lat": lat}
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        response.raise_for_status()
+
 # Example usage:
 if __name__ == "__main__":
     url = "http://localhost:8000/generate-recommendation"
-    # text_result = hit_text_recommendation(url, "Nama: Alif Ramadhan, aku laper banget pengen makan yang manis-manis dengan budget 50$")
+    text_result = hit_text_recommendation(url, "Nama: Alif Ramadhan, aku laper banget pengen makan yang manis-manis dengan budget 50$")
     text_result = hit_text_recommendation(url, "Nama: Alif Ramadhan, aku laper banget pengen makan")
-    # image_result = hit_image_recommendation(url, "static/keju.jpg")
-    # multimodal_result = hit_multimodal_recommendation(
-    #     url, 
-    #     query="Berikan rekomendasi makanan terkait gambar yang diberikan", 
-    #     file_path="static/keju.jpg"
-    # )
+    image_result = hit_image_recommendation(url, "static/keju.jpg")
+    multimodal_result = hit_multimodal_recommendation(url, query="Berikan rekomendasi makanan terkait gambar yang diberikan", file_path="static/keju.jpg")
+    geo_result = hit_transportation_recommendation(url, query="Pengen jalan-jalan ke monas, enaknya naik apa yaa")
